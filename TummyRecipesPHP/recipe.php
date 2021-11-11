1e9afda08451e4d313a31c2d83e9be275bee5496
JerielCastro <jerielcastro87@gmail.com>
2021-11-12 00:07:46 +0800

    Update recipe.php
    Added a "View More" so users can click for the full info of the recipe.

--- TummyRecipesPHP/recipe.php
+++ TummyRecipesPHP/recipe.php
@@ -23,55 +23,62 @@
             <section id="Pasta">
                 <h2>Pasta</h2>
                 <div class="row">
+                    
                 <article class="col-sm">
-                    <h3>Mac & Cheese</h3>
-                    <figure>   
-                                                                       
-                        <img class="img-thumbnail" 
-                             src="images/Mac and Cheese.jpg"
-                             alt="Mac & Cheese"
-                             title="Mac & Cheese"/>                     
-                        
-                       <figcaption>Mac & Cheese</figcaption>                                             
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Mac & Cheese</h3>
+                <figure>                                                                          
+                    <img class="img-thumbnail" 
+                         src="images/Mac and Cheese.jpg"
+                         alt="Mac & Cheese"
+                         title="Mac & Cheese"/>                         
+                </figure>
+                </a>
                     <p>
                         What to put here....
                         <br>put a link with "Press for Full Recipe"
                         <br>then it redirects to a page for the recipe?
+                        <a class="view-more" href="#">
+                            <br>View More
+                        </a>
                     </p>
                 </article>
                     
                 <article class="col-sm">
-                    <h3>Carbonara</h3>
-                    <figure>
-
-                        <img class="img-thumbnail" 
-                             src="images/Carbonara.jpg" 
-                             alt="Carbonara"
-                             title="Carbonara"/>
-                       
-                       <figcaption>Carbonara</figcaption>
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Carbonara</h3>
+                <figure>
+                    <img class="img-thumbnail" 
+                         src="images/Carbonara.jpg" 
+                         alt="Carbonara"
+                         title="Carbonara"/>
+                </figure>
+                </a>    
                     <p>
-                        Dont know what to put yet
-                        <br>thinking of making this scroll-able 
-                        from left to right?
+                        To Do:
+                        <br> 1)"View More" redirection link
+                        <br> 2)Dropdown nav for recipe list
+                        <a class="view-more" href="#">
+                            <br>View More
+                        </a>
                     </p>
                 </article>
                     
-                    <article class="col-sm">
-                    <h3>Lasagna</h3>
-                    <figure>
-                        
-                           <img class="img-thumbnail" 
-                                src="images/Lasagna.jpg" 
-                                alt="Lasagna"
-                                title="Lasagna"/>
-                           
-                        <figcaption>Lasagna</figcaption>
-                   </figure>
+                <article class="col-sm">
+                <a href="#">
+                <h3 class="text-center">Lasagna</h3>
+                <figure>           
+                    <img class="img-thumbnail" 
+                         src="images/Lasagna.jpg" 
+                         alt="Lasagna"
+                         title="Lasagna"/>                       
+                </figure>
+                </a>
                     <p>
-                       Shall add the link to full recipe next time
+                        Shall add the link to full recipe next time
+                        <a class="view-more" href="#">
+                            <br>View More
+                        </a>
                     </p>
                 </article>
                     
@@ -79,115 +86,127 @@
             </section>
             
             <section id="Rice">
-                <h2>Rice</h2>
-                <div class="row">
+            <h2>Rice</h2>
+            <div class="row">
                     
                 <article class="col-sm">
-                    <h3>Garlic Butter Rice</h3>
-                    <figure>
-
-                            <img class="img-thumbnail" 
-                                 src="images/Garlic Butter Rice.jpg" 
-                                 alt="Garlic Butter Rice"
-                                 title="Garlic Butter Rice"/>
-                       
-                        
-                        <figcaption>Garlic Butter Rice</figcaption>
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Garlic Butter Rice</h3>
+                <figure>
+                    <img class="img-thumbnail" 
+                         src="images/Garlic Butter Rice.jpg" 
+                         alt="Garlic Butter Rice"
+                         title="Garlic Butter Rice"/>                                          
+                </figure>
+                </a>
                     <p>
                        Maybe put description of the food i guess...
+                       <a class="view-more" href="#">
+                           <br>View More
+                       </a>
                     </p>
                 </article>
                     
                 <article class="col-sm">
-                    <h3>Shrimp Fried Rice</h3>
-                    <figure>
-                        
-                           <img class="img-thumbnail" 
-                                src="images/Shrimp Fried Rice.jpg" 
-                                alt="Shrimp Fried Rice"
-                                title="Shrimp Fried Rice"/>
-                           
-                        <figcaption>Shrimp Fried Rice</figcaption>
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Shrimp Fried Rice</h3>
+                <figure>                        
+                    <img class="img-thumbnail" 
+                         src="images/Shrimp Fried Rice.jpg" 
+                         alt="Shrimp Fried Rice"
+                         title="Shrimp Fried Rice"/>                                                   
+                </figure>
+                </a>
                     <p>
                        more to come i guess
+                       <a class="view-more" href="#">
+                           <br>View More
+                       </a>
                     </p>
                 </article>
                     
-                    <article class="col-sm">
-                    <h3>Kimchi Fried Rice</h3>
-                    <figure>
-                        
-                           <img class="img-thumbnail" 
-                                src="images/Kimchi Fried Rice.jpg" 
-                                alt="Kimchi Fried Rice"
-                                title="Kimchi Fried Rice"/>
-                           
-                        <figcaption>Kimchi Fried Rice</figcaption>
-                   </figure>
+                <article class="col-sm">
+                <a href="#">
+                <h3 class="text-center">Kimchi Fried<br>Rice</h3>
+                <figure>
+                    <img class="img-thumbnail" 
+                         src="images/Kimchi Fried Rice.jpg" 
+                         alt="Kimchi Fried Rice"
+                         title="Kimchi Fried Rice"/>                    
+                </figure>
+                </a>
                     <p>
                        Testing to see if images resize
+                       <a class="view-more" href="#">
+                           <br>View More>
+                       </a>
                     </p>
                 </article>
-                </div>
+                
+            </div>
             </section>
             
             <section id="Chicken">
-                <h2>Chicken</h2>
-                <div class="row">
+            <h2>Chicken</h2>
+            <div class="row">
                     
                 <article class="col-sm">
-                    <h3>Filipino Chicken Adobo</h3>
-                    <figure>
-
-                            <img class="img-thumbnail" 
-                                 src="images/Chicken Adobo.jpg" 
-                                 alt="Chicken Adobo"
-                                 title="Chicken Adobo"/>
-                       
-                        
-                        <figcaption>Chicken Adobo</figcaption>
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Filipino Chicken Adobo</h3>
+                <figure>
+                    <img class="img-thumbnail" 
+                         src="images/Chicken Adobo.jpg" 
+                         alt="Chicken Adobo"
+                         title="Chicken Adobo"/>                                                                      
+                </figure>
+                </a>
                     <p>
                        This is making me hungry while doing this at 3am
+                       <a class="view-more" href="#">
+                           <br>View More
+                       </a>
                     </p>
                 </article>
                     
                 <article class="col-sm">
-                    <h3>Garlic Butter Chicken</h3>
-                    <figure>
-                        
-                           <img class="img-thumbnail" 
-                                src="images/Garlic Butter Chicken.jpg" 
-                                alt="Garlic Butter Chicken"
-                                title="Garlic Butter Chicken"/>
-                           
-                        <figcaption>Garlic Butter Chicken</figcaption>
-                   </figure>
+                <a href="#">
+                <h3 class="text-center">Garlic Butter Chicken</h3>
+                <figure>                        
+                    <img class="img-thumbnail" 
+                         src="images/Garlic Butter Chicken.jpg" 
+                         alt="Garlic Butter Chicken"
+                         title="Garlic Butter Chicken"/>                                                  
+                </figure>
+                </a>
                     <p>
                        Super hungry
+                       <a class="view-more" href="#">
+                           <br>View More
+                       </a>
                     </p>
                 </article>
                     
-                    <article class="col-sm">
-                    <h3>Mediterranean Chicken</h3>
-                    <figure>
-                        
-                           <img class="img-thumbnail" 
-                                src="images/Mediterranean Chicken Skillet.jpg" 
-                                alt="Mediterranean Chicken"
-                                title="Mediterranean Chicken"/>
-                           
-                        <figcaption>Mediterranean Chicken</figcaption>
-                   </figure>
+                <article class="col-sm">
+                <a href="#">
+                <h3 class="text-center">Mediterranean Chicken</h3>
+                <figure>                       
+                    <img class="img-thumbnail" 
+                         src="images/Mediterranean Chicken Skillet.jpg" 
+                         alt="Mediterranean Chicken"
+                         title="Mediterranean Chicken"/>
+                </figure>
+                </a>
                     <p>
                        uhhh might be a problem if the length of name is diff
                        cuz the images wont be aligned.
                        <br> need to cite images!!
+                       <a class="view-more" href="#">
+                           <br>View More
+                       </a>
                     </p>
                 </article>
-                </div>
+                
+            </div>
             </section>
         </main>
         
