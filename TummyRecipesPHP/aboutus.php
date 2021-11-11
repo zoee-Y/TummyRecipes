5c245421dc462904815be0a95cd7d7929a4a92e6
Ariff Sanip <ariffsanip@gmail.com>
2021-11-11 14:01:53 +0800

    Created head.inc.php
    edited on the index.php and aboutus.php to add head.inc.php. I think this would be easier for us if need to add any new css or js link. can just edit the head.inc.php

--- TummyRecipesPHP/aboutus.php
+++ TummyRecipesPHP/aboutus.php
@@ -2,33 +2,10 @@
 
 <html lang="en">
     
-    <head>
-        <title>Tummy Recipes</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--jQuery-->
-        <script defer
-                src="https://code.jquery.com/jquery-3.4.1.min.js"
-                integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-                crossorigin="anonymous">
-        </script>
-        <script defer src="js/mainpage.js"></script>
-        <!--Bootstrap JS-->
-        <script defer
-                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-                integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-                crossorigin="anonymous">
-        </script>
-        <!--CSS-->
-        <link rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-              integrity=
-              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-              crossorigin="anonymous">
-        <link rel="stylesheet" href="css/navbar_footer.css">
-        <link rel="stylesheet" href="css/mainpage.css">
-        <link rel="stylesheet" href="css/aboutus.css">
-    </head>
+    <!-- Head -->
+    <?php
+    include "head.inc.php";
+    ?>
     
     <body>
         <!--Navbar-->
@@ -36,7 +13,6 @@
         include "nav.inc.php";
         ?>
         
-        <!--Main Content-->
         <!--Main Content-->
         <main class="container">
             <!-- About US -->
--- TummyRecipesPHP/index.php
+++ TummyRecipesPHP/index.php
@@ -2,45 +2,10 @@
 
 <html lang="en">
     
-    <head>
-    <meta charset= UTF-8>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Google Material Icon -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    
-    <!-- Remixicon -->
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" 
-           href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-           crossorigin="anonymous">
-            
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="Css/navbar_footer.css">
-    <link rel="stylesheet" href="Css/mainpage.css">
-
-    <!--jQuery-->
-    <script defer
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
-    </script>
-
-    <!--Bootstrap JS-->
-    <script defer
-        src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-        crossorigin="anonymous">
-    </script>
-
-    <!-- Custom JS -->
-    <script defer src="js/main.js"></script>
-
-    <title>Tummy Recipes</title>
-    </head>
+    <!-- Head -->
+    <?php
+    include "head.inc.php";
+    ?>
     
     <body>
         <!--Navbar-->
