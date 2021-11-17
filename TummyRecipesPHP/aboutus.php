724d8f94917014d38b202443be0a1f104c86f494
Ariff Sanip <ariffsanip@gmail.com>
2021-11-17 10:01:20 +0800

    About US
    Updated the About US section. You guys can check it out, if its okay, we can use it. If you guys want to edit here and there also can. Also can upload your preferred images lah hahahaha.

--- TummyRecipesPHP/aboutus.php
+++ TummyRecipesPHP/aboutus.php
@@ -26,18 +26,68 @@
                             <img class="aboutusimg" src="images/sampleimg1.jpeg" alt="AboutUs" name="TheTeam">
                         </figure>
                         <p>
-                        The team is made up of 5 wonderful individuals with 
-                        an extreme passion for food and making them fast!
+                            The team is made up of 5 wonderful individuals with
+                            an extreme passion for food and making them fast!
+                        </p>
+                    </article>
+                </div>
+                <div class="row">
+                    <article class="col-sm">
+                        <h4 class="nameDisplay">Zoe</h4>
+                        <figure>
+                            <img class="aboutus" src="images/sampleimg1.jpeg" alt="Zoe" name="ZoeIMG">
+                        </figure>
+                        <p>
+                            The food enthusiast!<br>
+                            No quit persona that will do anything in her power<br>
+                            to give us good food!
                         </p>
                     </article>
                     <article class="col-sm">
-                        <h4>Zoe</h4>
+                        <h4 class="nameDisplay">Ariff</h4>
                         <figure>
-                            <img class="aboutusimg" src="images/sampleimg1.jpeg" alt="Zoe" name="ZoePhoto">
+                            <img class="aboutus" src="images/Ariff.jpg" alt="Ariff" name="AriffIMG">
                         </figure>
                         <p>
-                        The team is made up of 5 wonderful individuals with 
-                        an extreme passion for food and making them fast!
+                            The tummy tumbler!<br>
+                            Always on the lookout for the best Mee Soto around.<br>
+                            Not gonna quit until he finds the best!
+                        </p>
+                    </article>
+                    <article class="col-sm">
+                        <h4 class="nameDisplay">JiaXin</h4>
+                        <figure>
+                            <img class="aboutus" src="images/sampleimg1.jpeg" alt="JiaXin" name="JiaXinIMG">
+                        </figure>
+                        <p>
+                            The Cooking Mama!<br>
+                            Gamer turned foodie.<br>
+                            As the name suggest, do not challenge her in a cooking contest.
+                        </p>
+                    </article>
+                    <article class="col-sm">
+                        <h4 class="nameDisplay">Jeriel</h4>
+                        <figure>
+                            <img class="aboutus" src="images/sampleimg1.jpeg" alt="Jeriel" name="JerielIMG">
+                        </figure>
+                        <p>
+                            The Cookie Monster!<br>
+                            You got bakes?<br>
+                            He's our cookie connoisseur.<br>
+                            Do not be intimidate by his inner cookie demon.<br>
+                            Just hand him a nice piece of baked cookie, and he'll be subdued.
+                        </p>
+                    </article>
+                    <article class="col-sm">
+                        <h4 class="nameDisplay">YuXun</h4>
+                        <figure>
+                            <img class="aboutus" src="images/sampleimg1.jpeg" alt="YuXun" name="YuXunIMG">
+                        </figure>
+                        <p>
+                            The Sweet Tooth!<br>
+                            If sugar rush has a baby, you're looking at him.<br>
+                            Be careful of what you're giving him.<br>
+                            One wrong dessert and he'll go Sugar Berserk.
                         </p>
                     </article>
                 </div>
