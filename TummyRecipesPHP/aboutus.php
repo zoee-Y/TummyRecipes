a91205a32c7c6582b4ccd00b4fc052aaa1029baa
Ariff Sanip <ariffsanip@gmail.com>
2021-11-18 17:22:03 +0800

    About Us
    made changes to the arrangement class of the article section, readability seems better this way as compared to the col-sm class.

--- TummyRecipesPHP/aboutus.php
+++ TummyRecipesPHP/aboutus.php
@@ -31,60 +31,60 @@
                         </p>
                     </article>
                 </div>
-                <div class="row">
-                    <article class="col-sm">
+                <div class="col">
+                    <article class="row-sm">
                         <h4 class="nameDisplay">Zoe</h4>
                         <figure>
                             <img class="aboutus" src="images/sampleimg1.jpeg" alt="Zoe" name="ZoeIMG">
+                            <figcaption>The food enthusiast!</figcaption>
                         </figure>
-                        <p>
-                            The food enthusiast!<br>
+                        <p class="teaminfo">
                             No quit persona that will do anything in her power<br>
                             to give us good food!
                         </p>
                     </article>
-                    <article class="col-sm">
+                    <article class="row-sm">
                         <h4 class="nameDisplay">Ariff</h4>
                         <figure>
                             <img class="aboutus" src="images/Ariff.jpg" alt="Ariff" name="AriffIMG">
+                            <figcaption>The tummy tumbler!</figcaption>
                         </figure>
-                        <p>
-                            The tummy tumbler!<br>
+                        <p class="teaminfo">
                             Always on the lookout for the best Mee Soto around.<br>
                             Not gonna quit until he finds the best!
                         </p>
                     </article>
-                    <article class="col-sm">
+                    <article class="row-sm">
                         <h4 class="nameDisplay">JiaXin</h4>
                         <figure>
                             <img class="aboutus" src="images/sampleimg1.jpeg" alt="JiaXin" name="JiaXinIMG">
+                            <figcaption>The Cooking Mama!</figcaption>
                         </figure>
-                        <p>
-                            The Cooking Mama!<br>
+                        <p class="teaminfo">
                             Gamer turned foodie.<br>
                             As the name suggest, do not challenge her in a cooking contest.
                         </p>
                     </article>
-                    <article class="col-sm">
+                    <article class="row-sm">
                         <h4 class="nameDisplay">Jeriel</h4>
                         <figure>
                             <img class="aboutus" src="images/sampleimg1.jpeg" alt="Jeriel" name="JerielIMG">
+                            <figcaption>The Cookie Monster!</figcaption>
                         </figure>
-                        <p>
-                            The Cookie Monster!<br>
+                        <p class="teaminfo">
                             You got bakes?<br>
                             He's our cookie connoisseur.<br>
                             Do not be intimidate by his inner cookie demon.<br>
                             Just hand him a nice piece of baked cookie, and he'll be subdued.
                         </p>
                     </article>
-                    <article class="col-sm">
+                    <article class="row-sm">
                         <h4 class="nameDisplay">YuXun</h4>
                         <figure>
                             <img class="aboutus" src="images/sampleimg1.jpeg" alt="YuXun" name="YuXunIMG">
+                            <figcaption>The Sweet Tooth!</figcaption>
                         </figure>
-                        <p>
-                            The Sweet Tooth!<br>
+                        <p class="teaminfo">
                             If sugar rush has a baby, you're looking at him.<br>
                             Be careful of what you're giving him.<br>
                             One wrong dessert and he'll go Sugar Berserk.
