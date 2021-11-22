9144d8eb45862e339fac367e5c1862f70df8eb2d
Umkayy <mandyjiaxin98@gmail.com>
2021-11-23 05:04:20 +0800

    Update footer.inc.php
    added logo but havent link

--- TummyRecipesPHP/footer.inc.php
+++ TummyRecipesPHP/footer.inc.php
@@ -1,7 +1,13 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<!-- Head -->
+    <?php
+    include "head.inc.php";
+    ?>
 <footer class="container">
+    <br>
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <br>
     <br>
     <p><em>Copyright &copy; 2021 Tummy Recipes Pte. Ltd.</em></p>
 </footer>
