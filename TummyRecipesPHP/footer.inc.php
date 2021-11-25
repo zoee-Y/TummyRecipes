660b8eeffabc5c2ce7ddcc8fb403f98ba99a25b8
Umkayy <mandyjiaxin98@gmail.com>
2021-11-25 22:53:40 +0800

    Update footer.inc.php

--- TummyRecipesPHP/footer.inc.php
+++ TummyRecipesPHP/footer.inc.php
@@ -5,8 +5,8 @@
     ?>
 <footer class="container">
     <br>
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-instagram"></a>
+    <a href="https://m.facebook.com/Tummy-Recipes-101999638991593/" class="fa fa-facebook"></a>
+    <a href="https://instagram.com/tummyrecipes?utm_medium=copy_link" class="fa fa-instagram"></a>
     <br>
     <br>
     <p><em>Copyright &copy; 2021 Tummy Recipes Pte. Ltd.</em></p>
