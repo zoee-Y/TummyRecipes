ee4b086cced0b63b11bfdff67444ab6d34b72dca
JerielCastro <jerielcastro87@gmail.com>
2021-11-27 00:32:25 +0800

    minor tweaks

--- TummyRecipesPHP/MyCookbookSession.php
+++ TummyRecipesPHP/MyCookbookSession.php
@@ -37,10 +37,7 @@
                                 <h6>Mac & Cheese</h6>
 
                             </a>
-                            <p style="text-align: justify;"> Macaroni and cheese 
-                            is a pasta dish made with cooked macaroni 
-                            and a cheese sauce, usually cheddar. 
-                            Other ingredients, such as breadcrumbs<br>...
+                         
                                 <a class="view-more" href="Mac and Cheese.php">
                                     <br>Press me for Recipes!
                                 </a>
@@ -62,11 +59,7 @@
                                 <h6>Shrimp Fried Rice</h6>
 
                             </a>
-                            <p style="text-align: justify;">In Chinese takeaway restaurants, 
-                            shrimp fried rice is always one of the most popular fried rice meals. 
-                            Fried rice is typically dark brown in color, due to a combination of dark 
-                            and light soy sauce and <br>...
-                               
+                            
                                <a class="view-more" href="Shrimp Fried Rice.php">
                                    <br>Press me for Recipes!
                                </a>
@@ -88,11 +81,6 @@
                                 <h6>Garlic Butter Chicken</h6>
 
                             </a>
-                            <p style="text-align: justify;">This warm garlic butter 
-                            chicken breast recipe will become a top choice if you're 
-                            looking for a quick and simple 15-minute chicken dinner. 
-                            It's done in a single skillet, 
-                            which makes cleaning a breeze <br>...
                                
                                <a class="view-more" href="Garlic Butter Chicken.php">
                                    <br>Press me for Recipes!
