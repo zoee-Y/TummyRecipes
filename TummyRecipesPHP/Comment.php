4f9dd278a2f307c0f78174b1fc2599f3cbc86dcb
yuxun19999 <63785913+yuxun19999@users.noreply.github.com>
2021-11-27 01:35:29 +0800

    Comment update

--- TummyRecipesPHP/Comment.php
+++ TummyRecipesPHP/Comment.php
@@ -1,22 +1,54 @@
 
 <?php
-include "Timezone.php";
-//include "dbh.inc.php";
-//include "Comment_function.php";
+include "Commenting_function.php";
+
+$message = $errorMsg = "";
+$success = true;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['commentSubmit']))
+{
+    // validate if email is empty
+    if (empty($_POST["message"]))
+    {
+        $errorMsg .= "Comment is required.<br>";
+        $success = false;
+    }
+    else
+    {
+        $message = ($_POST["message"]);
+        $uid = ($_POST["uid"]);
+        $date = ($_POST["date"]);
+    }
+    
+    
+    
+    // If no error, authenticate user!
+    if (empty($errorMsg))
+    {
+        setComments();
+        getComments();
+    }
+}
 ?>
 
 <link rel='stylesheet' type='text/css' href='css/Comment.css'>
 
 
+<!--
+<div class='form-group mb-3'>
+    <input type =text value='<?php = $rowvalue['message'];?>'>    
+</div> -->
 
 
-<form method='post' action="Commenting_function.php">
- <!--To be added in afterwards 
-<form> -->
-    <input type='hidden' name='uid' value='Anonymous'>
-    <input type='hidden' name='date' value =".date('Y-m-d H:i:s').">
-    <textarea name='message'></textarea><br>
-    <button name='submit' type='commentSubmit'>Comment</button>
+<form  method="post">
+    <div class="form-group">
+        <label for="message">Comment:</label> 
+        <input class="form-control" type="text" id="message" 
+               name="message" placeholder="Enter your comment">
+    </div>
+     <div class="form-group">
+        <button class="btn btn-primary" type="submit" name="commentSubmit">Comment</button>
+    </div>
 </form>
 
 
--- TummyRecipesPHP/Commenting_function.php
+++ TummyRecipesPHP/Commenting_function.php
@@ -1,55 +1,69 @@
 <?php
-$uid = $message = $errorMsg = "";
-$date = 0;
-$success = true;
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['commentSubmit'])) 
-{
-    if (empty($_POST["message"]))
-    {
-        $errorMsg .= "Comment is required.<br>";
-        $success = false;
-    }
-    else
-    {
-        $message = ($_POST["message"]);
-        $uid = ($POST["uid"]);
-        $date = ($POST["date"]);
-        setComments();
-    }
-    
-}
 
 
+ 
+// Helper function that a
 function setComments()
 {
-    global $message,$uid,$date;
+    global $message, $errorMsg, $success;
     
+    // Create database connection.
     $config = parse_ini_file('../../private/db-config.ini');
     $conn = new mysqli($config['servername'], $config['username'],
     $config['password'], $config['dbname']);
     
-     if ($conn->connect_error)
+    // Check connection
+    if ($conn->connect_error)
     {
         $errorMsg = "Connection failed: " . $conn->connect_error;
         $success = false;
     }
     else
     {
-        $stmc = $conn->prepare("INSERT INTO tummy_recipes_comments (uid,date,message) VALUES (?, ?, ?)");
-        $stmc->bind_param("sis", $uid,$date,$message); //not adding img yet
+        // Prepare the statement:
+        $stmt = $conn->prepare("INSERT INTO tummy_recipes_comment_test (message) VALUES (?)");
         
-        if (!$stmc->execute()) {
-            $errorMsg = "Adding of comments failed.";
+        // Bind & execute the query statement:
+        $stmt->bind_param("s",$message);
+        if (!$stmt->execute())
+        {
+            $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
             $success = false;
         }
-        else
-        {
-            $stmc->close();
-            
-        }
-        
-                
+        $stmt->close();
     }
+    
     $conn->close();
 }
+
+function getComments() 
+{
+    global $message, $errorMsg, $success;
+    
+    // Create database connection.
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'],
+    $config['password'], $config['dbname']);
+    
+    // Check connection
+    if ($conn->connect_error)
+    {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    }
+    else
+    {
+        
+        $stmt = $conn->prepare("SELECT * FROM tummy_recipes_comment_test WHERE message=?");
+        $stmt->bind_param("s", $message);
+        $stmt->execute();
+        $result = mysql_query($stmt);
+        
+        while ($rowvalue= mysql_fetch_array($result)){
+            $message = $rowvalue['message'];
+        }
+    $conn->close();
+    }   
+}
+
+?>
