3b60af71154964b94a44c864a125cf95f4e8fc64
yuxun19999 <63785913+yuxun19999@users.noreply.github.com>
2021-11-28 02:18:28 +0800

    Update Comment_Home_Carbonara.php

--- TummyRecipesPHP/Comment_Home_Carbonara.php
+++ TummyRecipesPHP/Comment_Home_Carbonara.php
@@ -1,65 +1,67 @@
 <?php
 
-$message = $errorMsg = "";
-$success = true;
+    session_start();
+    
+    $message = $errorMsg = "";
+    $success = true;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['commentSubmit']))
-{
-    // validate if email is empty
-    if (empty($_POST["message"]))
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['commentSubmit']))
     {
-        $errorMsg .= "Comment is required.<br>";
-        $success = false;
-    }
-    else
-    {
-        $message = ($_POST["message"]);
-        $uid = ($_POST["uid"]);
-        $date = ($_POST["date"]);
-    }
-    
-    
-    
-    // If no error, authenticate user!
-    if (empty($errorMsg))
-    {
-        setComments();
-       
-    }
-}
-
-function setComments()
-{
-    global $message, $errorMsg, $success;
-    
-    // Create database connection.
-    $config = parse_ini_file('../../private/db-config.ini');
-    $conn = new mysqli($config['servername'], $config['username'],
-    $config['password'], $config['dbname']);
-    
-    // Check connection
-    if ($conn->connect_error)
-    {
-        $errorMsg = "Connection failed: " . $conn->connect_error;
-        $success = false;
-    }
-    else
-    {
-        // Prepare the statement:
-        $stmt = $conn->prepare("INSERT INTO tummy_recipes_comment_carbonara (message) VALUES (?)");
-        
-        // Bind & execute the query statement:
-        $stmt->bind_param("s",$message);
-        if (!$stmt->execute())
+        // validate if email is empty
+        if (empty($_POST["message"]))
         {
-            $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            $errorMsg .= "Comment is required.<br>";
             $success = false;
         }
-        $stmt->close();
+        else
+        {
+            $message = ($_POST["message"]);
+            $uid = ($_POST["uid"]);
+            $date = ($_POST["date"]);
+        }
+
+
+
+        // If no error, authenticate user!
+        if (empty($errorMsg))
+        {
+            setComments();
+
+        }
     }
-    
-    $conn->close();
-}
+
+    function setComments()
+    {
+        global $message, $errorMsg, $success;
+
+        // Create database connection.
+        $config = parse_ini_file('../../private/db-config.ini');
+        $conn = new mysqli($config['servername'], $config['username'],
+        $config['password'], $config['dbname']);
+
+        // Check connection
+        if ($conn->connect_error)
+        {
+            $errorMsg = "Connection failed: " . $conn->connect_error;
+            $success = false;
+        }
+        else
+        {
+            // Prepare the statement:
+            $stmt = $conn->prepare("INSERT INTO tummy_recipes_comment_carbonara (message) VALUES (?)");
+
+            // Bind & execute the query statement:
+            $stmt->bind_param("s",$message);
+            if (!$stmt->execute())
+            {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                $success = false;
+            }
+            $stmt->close();
+        }
+
+        $conn->close();
+    }
 
 ?>
 
