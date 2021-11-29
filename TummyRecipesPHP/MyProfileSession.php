9149130eb73db1c0b3644a274eabb024f0e66146
Ariff Sanip <ariffsanip@gmail.com>
2021-11-29 22:34:58 +0800

    Update MyProfileSession.php

--- TummyRecipesPHP/MyProfileSession.php
+++ TummyRecipesPHP/MyProfileSession.php
@@ -95,7 +95,7 @@
                                     $conn->close();
                                 ?>
                             </div>
-                            <!-- Edit/Delete Profile -->
+                            <!-- Edit Profile -->
                             <p align="center">
                                 <a class="edit-profile" href="MyProfileinfo.php">
                                 <br><h4>Edit Profile</h4></a>
