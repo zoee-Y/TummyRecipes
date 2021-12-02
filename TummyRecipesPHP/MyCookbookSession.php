b20efb831c307a1f94d84ae029f8db67ad777272
Ariff Sanip <ariffsanip@gmail.com>
2021-12-02 20:06:13 +0800

    Update MyCookbookSession.php

--- TummyRecipesPHP/MyCookbookSession.php
+++ TummyRecipesPHP/MyCookbookSession.php
@@ -46,6 +46,7 @@
                         while ($row = mysqli_fetch_assoc($query_reps)) {
                             
                             $imgThumb = $row['imgThumbnail'];
+                            $foodTitle = $row['rTitle'];
                             $hours = $row['hours'];
                             $minutes = $row['minutes'];
                             $ingredients = $row['ingredients'];
@@ -54,24 +55,21 @@
                             echo "<div class='row justify-content-around'>";
                             echo "<div class='row-sm'>";
                             echo "<article class='col-sm'>";
-                            
                             echo "<figure>";
-                            // Button with class 'recipeInfo'
+                            
+                            // Thumbnail and Recipe Title as Button
                             echo "<button class='recipeInfo'>";
                             echo "<p class='recipeTog' style='text-align: center;'>";
-                            echo "<img src='".$row["imgThumbnail"]."' style='border-radius: 50%; width: 200px;' alt='".$row["rTitle"]."'>";
-                            echo "<h4 style='text-align: center;'>" . $row['rTitle'] . "</h4>";
+                            echo "<img src='". $imgThumb ."' style='border-radius: 50%; width: 200px;' alt='".$row["rTitle"]."'>";
+                            echo "<h4 style='text-align: center;'>". $foodTitle ."</h4>";
                             echo "</p>";
                             echo "</button>";
                             echo "</figure>";
                             
-                            // p with class 'recipeTog'
-                            //echo "<p class='recipeTog' style='font-size: 15px'>";
+                            // Duration Output
                             echo "<h4>";
                             echo "<p class='recipeTog'><b><u>Duration</u></b></p>";
-                            //echo "</p>";
                             echo "</h4>";
-                            //echo "<p class='recipeTog'>";
                             if ($hours > 0) {
                                 echo "<h6><p class='recipeTog'>". $hours ." hour(s)";
                                 if ($minutes > 0) {
@@ -79,46 +77,34 @@
                                 }
                             }
                             else if ($minutes > 0) {
-                                echo "<h6><p class='recipeTog'>$minutes minutes</p></h6>";
+                                echo "<h6><p class='recipeTog'>". $minutes ." minutes</p></h6>";
                             }
-                            //echo "</p>";
                             echo "<br>";
                             echo "<br>";
-                            //echo "<p class='recipeTog' style='font-size: 15px'>";
+                            
+                            // Ingredients output
                             echo "<h4>";
                             echo "<p class='recipeTog'><b><u>Ingredients</u></b></p>";
                             echo "</h4>";
-                            //echo "</p>";
-                            //echo "<p class='recipeTog'>";
-                            //echo "<h6>";
                             $uns = unserialize($ingredients);
 
                             for ($i = 0; $i < count($uns); $i++) {
-                                echo "<h6><p class='recipeTog'>$uns[$i]</p></h6>";
+                                echo "<h6><p class='recipeTog'>". $uns[$i] ."</p></h6>";
                             }
-                            //echo "</h6>";
-                            //echo "</p>";
                             echo "<br>";
                             echo "<br>";
-                            //echo "<p class='recipeTog' style='font-size: 15px'>";
+                            
+                            // Instructions Output (Steps)
                             echo "<h4>";
                             echo "<p class='recipeTog'><b><u>Instructions</u></b></p>";
                             echo "</h4>";
-                            //echo "</p>";
-                            //echo "<p class='recipeTog'>";
-                            //echo "<h6>";
                             $unsS = unserialize($steps);
 
                             for ($i = 0; $i < count($unsS); $i++) {
-                                echo "<h6><p class='recipeTog'>$unsS[$i]</p></h6>";
+                                echo "<h6><p class='recipeTog'>". $unsS[$i] ."</p></h6>";
                             }
-                            //echo "</h6>";
-                            //echo "</p>";
-                            // End of p with class recipeTog
-                            
                             echo "<br>";
                             echo "<br>";
-                            //include "Display_Comment_Carbonara.php";
                             echo "</article>";
                             echo "</div>";
                             echo "</div>";
@@ -126,10 +112,6 @@
                     }
                     else {
                         echo "<h4>You have Not Uploaded Any Recipes Yet! Share your recipes Now!</h4>";
-                        //echo "<p align='center'>";
-                        // echo "<h4><a href='newrecipe.php' class='btn btn-success'>Add New Recipe!</a></h4>";
-                        //echo "</p>";
-                        // exit();
                     }
                     $query_reps->close();
                 }
